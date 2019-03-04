8aecca5bd4bbe611750c06d239357f4f0b8b2f70
Alan Thompson <alanthompson.se@gmail.com>
2019-03-04 18:27:47 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Contact from './components/Contact'
+import Header from './components/Header'
+//Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <div className="container">
+                    <Contact />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App
